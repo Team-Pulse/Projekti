67b39dccfa5c888184ea7b800662ab3ce8a24c1e
DESKTOP-KGAUOGM\Jukkis <jukka-pekka.lappalainen@outlook.com>
2020-09-28 12:51:16 +0300

    Uus luokka

---
+++ app/src/main/java/com/example/kalorilaskuri/FoodListDetailsSingletonClass.java
@@ -0,0 +1,26 @@
+package com.example.kalorilaskuri;
+
+import java.util.ArrayList;
+
+public class FoodListDetailsSingletonClass {
+    private static final FoodListDetailsSingletonClass ourInstance = new FoodListDetailsSingletonClass();
+    private ArrayList<Food> foodDetailsAcitiy;
+
+    private FoodListDetailsSingletonClass(){
+        foodDetailsAcitiy = new ArrayList<>();
+    }
+
+    public static FoodListDetailsSingletonClass getInstance(){
+        return ourInstance;
+    }
+
+    public ArrayList<Food> getFoods(){
+        return this.foodDetailsAcitiy;
+    }
+
+    public Food getFood(int index){
+        return foodDetailsAcitiy.get(index);
+    }
+
+
+}

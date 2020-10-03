dc41d6f08232888cc28ac95a15329ceefadc0826
DESKTOP-KGAUOGM\Jukkis <jukka-pekka.lappalainen@outlook.com>
2020-10-03 15:35:16 +0300

    Revert "SignUp system works!"
    This reverts commit b345e0b716ac3ad4428b68eb8101d05c2e7249c3.

--- app/src/main/java/com/example/kalorilaskuri/Login.java
+++ app/src/main/java/com/example/kalorilaskuri/Login.java
@@ -1,22 +1,13 @@
 package com.example.kalorilaskuri;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     private EditText name;
@@ -25,8 +16,6 @@ public class Login extends AppCompatActivity {
     private Button loginbutton;
     private int counter = 3;
     private TextView userRegisteration;
-    private FirebaseAuth firebaseAuth;
-    private ProgressDialog progressDialog;
 
 
     @Override
@@ -34,31 +23,13 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        name = (EditText)findViewById(R.id.etname);
-        password = (EditText) findViewById(R.id.etuserpassword);
-        info = (TextView)findViewById(R.id.tvinfo);
-        loginbutton = (Button)findViewById(R.id.btnlogin);
-        userRegisteration = (TextView)findViewById(R.id.tvregister);
-
-        info.setText("Number of attempts remaining 5");
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(this);//
-        FirebaseUser user = firebaseAuth.getCurrentUser();/*Kommunikoi databasen kanssa ja kertoo
-                                                            onko käyttäjä kirjautunut Firebaseen.*/
-        if (user == null){//jos kirjautunut sisään, tuhoaa aktiviteetin ja siiryy Mainclass.classiin.
-           finish();
-           startActivity(new Intent(Login.this, MainActivity.class));
-        }
 
 
+        setupUIViews();
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*Tarkistaa, täsmääkö käyttäjänimi ja salasana databaseen asetettuun käyttäjätiliin
-                * ja salasanaan.*/
                 validate(name.getText().toString(), password.getText().toString());
-
             }
         });
 
@@ -70,34 +41,35 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+    //asettaa muuttujille näkymät.
+    private  void setupUIViews(){
+        name = (EditText)findViewById(R.id.etname);
+        password = (EditText) findViewById(R.id.etuserpassword);
+        info = (TextView)findViewById(R.id.tvinfo);
+        loginbutton = (Button)findViewById(R.id.btnlogin);
+        userRegisteration = (TextView)findViewById(R.id.tvregister);
 
+    }
 
-    /*Jos tehtävä suoritettu, siirtää käyttäjän toiseen aktiviteettiin.*/
+    /*
+     *
+     * @param userName username jonka if lausekkeen oletusasetuksena "Admin"
+     * @param userPassword salasana jonka toimii oletuksena if lausekkeessa "1234"
+     */
+    private void validate(String userName, String userPassword){
+        //If lauseke joka määrittelee uuden aktiviteetin aukeamisen salasanan ja käyttäjä-
+        //tunnuksen oikeinkirjoituksen yhteydessä.
 
-    private void validate(String userName, String userPassword) {
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            counter --;
+            info.setText("Number of attempts remaining " + String.valueOf(counter));
 
-        progressDialog.setMessage("Hold on. We're logging you in.");
-        progressDialog.show();
-
-        firebaseAuth.signInWithEmailAndPassword(userName, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    progressDialog.dismiss();
-                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(Login.this, MainActivity.class));
-
-
-                }else{
-                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                    counter--;
-
-                    progressDialog.dismiss();
-                    if(counter == 0){
-                        loginbutton.setEnabled(false);
-                    }
-                }
+            if(counter == 0){
+                loginbutton.setEnabled(false);
             }
-        });
+        }
     }
 }
